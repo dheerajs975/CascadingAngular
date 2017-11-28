eadaa33dfcdb15ab80d7bbb691322193aceb45c0
dheerajs975 <33510644+dheerajs975@users.noreply.github.com>
2017-11-28 10:31:04 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(angular_cascading.Startup))]
+namespace angular_cascading
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
